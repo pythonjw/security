e5f6431a78aab2878c8922ad3c00324ce97faf75
pakhm <744684245@qq.com>
2019-12-09 21:11:38 +0800

    2019年12月9日21:11:29 home

--- src/main/java/com/pjw/mapper/MyUserDetailsMapper.java
+++ src/main/java/com/pjw/mapper/MyUserDetailsMapper.java
@@ -1,8 +1,12 @@
 package com.pjw.mapper;
 
+import com.pjw.entity.MyUserDetails;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface MyUserDetailsMapper {
-
+    List<MyUserDetails> selectUser(@Param("userName") String userName);
 }
--- src/main/java/com/pjw/service/MyUserDetailsService.java
+++ src/main/java/com/pjw/service/MyUserDetailsService.java
@@ -1,20 +1,34 @@
 package com.pjw.service;
 
+import com.pjw.entity.MyUserDetails;
+import com.pjw.mapper.MyUserDetailsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    MyUserDetailsMapper myUserDetailsMapper;
 
     //根据账号查询用户信息
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        //将来连接数据库根据账号查询用户信息
-        //暂时采用模拟方式
-        UserDetails userDetails= User.withUsername("pakhm").password("123").authorities("p1").build();
-        return userDetails;
+        List<MyUserDetails> list= myUserDetailsMapper.selectUser(userName);
+        if(list.size()==0){
+            return null;
+        }else{
+            MyUserDetails myUserDetails=list.get(0);
+            System.out.println(myUserDetails);
+            //将来连接数据库根据账号查询用户信息
+            //暂时采用模拟方式
+            UserDetails userDetails= User.withUsername(myUserDetails.getUserName()).password(myUserDetails.getPassWord()).authorities("p1").build();
+            return userDetails;
+        }
     }
 }
