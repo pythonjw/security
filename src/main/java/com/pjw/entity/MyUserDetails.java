2f1e0b839e4718d2efbb586fd10309f65b5d33f1
pakhm <744684245@qq.com>
2019-12-09 12:45:11 +0800

    a

---
+++ src/main/java/com/pjw/entity/MyUserDetails.java
@@ -0,0 +1,12 @@
+package com.pjw.entity;
+
+import lombok.Data;
+
+@Data
+public class MyUserDetails {
+    private String id;
+    private String userName;
+    private String passWord;
+    private String fullName;
+    private String mobile;
+}
