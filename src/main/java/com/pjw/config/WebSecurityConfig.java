ceef20445abd357f8d0584d27dd5b9b11c6f70b7
pakhm <744684245@qq.com>
2019-12-10 16:44:51 +0800

    a

--- src/main/java/com/pjw/config/WebSecurityConfig.java
+++ src/main/java/com/pjw/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -46,6 +47,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             // .loginPage("/login-page")//自定义登录地址
             //.loginProcessingUrl("/login-process")//自定义登录处理地址
             .successForwardUrl("/login-success")  //自定义登录成功的地址 ,controller应该是post
+        .and()
+        .sessionManagement()//会话控制，我们可以通过以下选项准确控制会话合适创建以及Spring Security如何与之交互
+                            //always:如果没有session存在就创建一个
+                            //ifRequired:默认值，如果需要就创建一个session（默认）登录时
+                            //never:springSecurity将不会创建session，但是如果应用中其他地方创建了session，那么springSecurity将不会创建session将会使用它
+                            //stateless:springSecurity将绝对不会创建session，也不使用session
+        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+        .and()
+        .logout()//????
+        //.logoutUrl("/logout2")//自定义退出登录地址
+        //.logoutSuccessUrl("/logout-success")//退出成功跳转地址
         ;
     }
 }
--- src/main/java/com/pjw/controller/Login.java
+++ src/main/java/com/pjw/controller/Login.java
@@ -1,5 +1,8 @@
 package com.pjw.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +22,24 @@ public class Login {
     @PostMapping("/login-process")
     public String loginProcess(){
         return "loginProcess";
+    }
+
+    @GetMapping("/get-user")
+    public String getUser(){
+        String userName=null;
+        //当前认证通过的用户身份
+        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        //用户身份
+        Object principal=authentication.getPrincipal();
+        if(principal==null){
+            userName="游客";
+        }
+        if(principal instanceof org.springframework.security.core.userdetails.UserDetails){
+            UserDetails userDetails=(UserDetails) principal;
+            userName=userDetails.getUsername();
+        }else{
+            userName=principal.toString();
+        }
+        return userName;
     }
 }
